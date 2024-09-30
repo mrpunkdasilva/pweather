9679ffbe3ff9043648b50d0edff6d8677e7467fe
MrNullus <gustavojs417@gmail.com>
2024-09-29 22:02:26 -0300

    start

---
+++ app/src/main/java/com/punkdomus/pweather/MainActivity.java
@@ -0,0 +1,47 @@
+package com.punkdomus.pweather;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.punkdomus.pweather.Adapters.HourlyAdapters;
+import com.punkdomus.pweather.Domains.Hourly;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapterHourly;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        ArrayList<Hourly> items = new ArrayList<Hourly>();
+
+        items.add(new Hourly("9 pm", 28, "cloudy"));
+        items.add(new Hourly("11 pm", 29, "sunny"));
+        items.add(new Hourly("12 pm", 30, "wind"));
+        items.add(new Hourly("1 am", 29, "rainy"));
+        items.add(new Hourly("2 am", 26, "storm"));
+
+        recyclerView = findViewById(R.id.view1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(
+                this,
+                LinearLayoutManager.HORIZONTAL,
+                false
+        ));
+
+        adapterHourly = new HourlyAdapters(items);
+        recyclerView.setAdapter(adapterHourly);
+    }
+
+}
